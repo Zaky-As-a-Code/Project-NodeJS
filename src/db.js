4bb873fed3ebaf5bb5354e357c5f1d384d376256
Ahmed Zaky <136256227+Zaky-As-a-Code@users.noreply.github.com>
2023-09-03 02:48:19 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "dbc",
+  host: "mysql-service.default.svc.cluster.local",
   user: "zaky",
   password: "zakypass",
   port: 3306,
